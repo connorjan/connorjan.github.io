515ba560123165dc3061a874b801be8f63a3744f
Connor Goldberg <connor@connorgoldberg.com>
2014-08-25 21:37:59 -0400

    Added support for private directory using htpasswd

--- fabfile.py
+++ fabfile.py
@@ -17,15 +17,16 @@ def push():
     run('unzip /var/www/%s/_site.zip -d /var/www/%s' % (domain, domain))
     run('rm -rf /var/www/%s/%s' % (domain, subdom))
     run('mv /var/www/%s/_site /var/www/%s/%s' % (domain, domain, subdom))
+    local('rm -rf _site.zip')
 
 #Pushes the entire _static folder to the static directory on the server
 def push_static():
-	local('rm -rf _static.zip')
 	local('zip -r _static _static')
 	run('rm -rf /var/www/%s/_static.zip /var/www/%s/static' % (domain,domain))
 	put('_static.zip', '/var/www/%s/_static.zip' % (domain))
 	run('unzip /var/www/%s/_static.zip -d /var/www/%s' % (domain, domain))
 	run('mv /var/www/%s/_static /var/www/%s/static' % (domain, domain))
+	local('rm -rf _static.zip')
 
 """
 Pushes a single file from the _static folder to the static directory on the server.
@@ -35,6 +36,7 @@ $ fab push_file:<filename>
 
 def push_file(fileName):
 	#sets working directories to the static directories
+
 	with lcd('_static'), cd('/var/www/%s/static/' % domain):
 		
 		local('mkdir temp')
@@ -57,3 +59,31 @@ def push_resume():
 			run('rm /var/www/%s/static/Resume.pdf' % domain)
 			put('Resume.pdf', '/var/www/%s/static/Resume.pdf' % domain)
 			run('cp /var/www/%s/static/Resume.pdf /var/www/%s/www/Resume.pdf' % (domain,domain))
+
+#Pushes the entire _static folder to the static directory on the server
+def push_private():
+	local('zip -r _private _private')
+	run('rm -rf /var/www/%s/private' % domain)
+	put('_private.zip', '/var/www/%s/_private.zip' % (domain))
+	run('unzip /var/www/%s/_private.zip -d /var/www/%s' % (domain, domain))
+	run('mv /var/www/%s/_private /var/www/%s/private' % (domain, domain))
+	run('rm -rf /var/www/%s/_private.zip' % domain)
+	local('rm -rf _private.zip')
+
+def push_file_private(fileName):
+	#sets working directories to the static directories
+
+	with lcd('_private'), cd('/var/www/%s/private/' % domain):
+		
+		local('mkdir temp')
+		local('cp %s temp' % fileName)
+		local('zip -r temp temp')
+		if exists('%s' % (fileName)):
+			#Then the file already exists in the static directory on the server
+			run('rm %s' % fileName)
+		put('temp.zip', '/var/www/%s/private/temp.zip' % domain)
+		run('unzip -j temp.zip')
+
+		#clean up local temp stuff
+		run('rm temp.zip')
+		local('rm -rf temp temp.zip')
